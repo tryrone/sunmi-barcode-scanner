4be9d7d5caefb9933ad2f58e9e67f5a3327efac7
tryrone <tegararuvwe@gmail.com>
2022-11-22 10:15:25 +0100

    displayed result

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import React from "react";
-import { NativeModules, Button } from "react-native";
+import React, { useState } from "react";
+import { NativeModules, Button, Text } from "react-native";
 
 const { CalendarModule, CameraScanner } = NativeModules;
 
 export default function App() {
+  const [result, setResult] = useState("");
   const onPress = async () => {
     try {
-      const result = await CameraScanner.launchScanner();
-      console.log("RES::", result);
+      const resultResp = await CameraScanner.launchScanner();
+      console.log("RES::", resultResp);
+      setResult(resultResp);
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <Button
-      title="Click to invoke your native module!"
-      color="#841584"
-      onPress={onPress}
-    />
+    <>
+      <Button
+        title="Click to invoke your native module!"
+        color="#841584"
+        onPress={onPress}
+      />
+
+      <Text>result: {result}</Text>
+    </>
   );
 }
